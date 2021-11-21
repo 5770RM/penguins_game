0362e950353d623447ae5afbb8acb01d9542e74e
Enjie Jin <01165903@pw.edu.pl>
2021-11-21 11:07:59 +0100

    Update evaluation.c

--- src/T3/evaluation.c
+++ src/T3/evaluation.c
@@ -1,10 +1,6 @@
 #include "stuctures.h"
 #include "gameplay_functions.h"
-int determine_winner(struct player* players, int n);
-{
 
-}
-void end_game() 
-{
+int determine_winner(struct player* players, int n) {
 
 }
