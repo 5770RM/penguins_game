f44f309b314667d659bf8ff07029b1df507b1957
mjedrze5 <01168836@pw.edu.pl>
2021-11-25 00:10:54 +0100

    Update placement.c

--- src/T3/placement.c
+++ src/T3/placement.c
@@ -5,18 +5,19 @@
 
 #define END_PP 1
 
-int placement_game_status(penguins);
+//int placement_game_status(penguins);
 
-int placement_possible(current_player, penguins);
+//int placement_possible(current_player, penguins);
 
 int get_placement_x();
 
 int get_placement_y();
 
-int valid_placement(board, x, y);
+//int valid_placement(board, x, y);
 
 // executing placement
 void execute_placement(struct board_tile** board, struct player* players, int curr_player, struct placement p) {
+	/*
 	int nbr_of_players = get_nbr_of_players(); // save number of players in variable
 	int nbr_of_penguins = get_nbr_of_penguins(); // save number of penguins in variable
 	while (placement_game_status(nbr_of_penguins) != END_PP) {
@@ -37,12 +38,14 @@ void execute_placement(struct board_tile** board, struct player* players, int cu
 				nbr_of_penguins--; // reduce number of penguins after every successful placement
 			}
 	}
+	*/
 }
 
 // check if there is left some penguins
 // arguments: number of penguins (integer)
 // returns: 1 if number of penguins equals 0; 0 otherwise 
-int placement_game_status(penguins) {
+int placement_game_status(struct board_tile** board, struct player* players) {
+	int penguins = 0;
 	if (penguins != 0) {
 		return 0;
 	}
@@ -54,7 +57,7 @@ int placement_game_status(penguins) {
 // check if current_player has penguins
 // arguments: current_player, number of penguins (integer)
 // returns: 1 if current players has penguins, 0 otherwise
-int placement_possible(current_player, penguins) {
+int placement_possible(struct board_tile** board, struct player* players, int curr_player) {
 
 }
 
@@ -79,9 +82,11 @@ int get_placement_y() {
 // check if player entered valid coordinate
 // arguments: board (map), X coordinate from get_placement_x function, Y coordinate from get_placement_y function
 // returns 1 if coords valid, 0 otherwise
-int valid_placement(board, coord_x, coord_y) {
+int valid_placement(struct board_tile** board, struct placement p) {
+	/*
 	if ((coord_x > 0) && (coord_y > 0) && (coord_x < board[x]) && (coord_y < board[y]) && board[coord_x][coord_y] == -1)
 		return 1;
 	else
 		return 0;
+	*/
 }
