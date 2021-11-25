0fc79efebabcb585f8d5ce1aae6cc40db520e756
mjedrze5 <01168836@pw.edu.pl>
2021-11-25 15:12:15 +0100

    Update gameplay_functions.h

--- src/T3/gameplay_functions.h
+++ src/T3/gameplay_functions.h
@@ -1,35 +1,42 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
+
+// asks user for the number of players
 // arguments: none
 // returns: number of players
 int get_nbr_of_players();
 
+// asks user for number of penguins each player will have at their disposal 
 // arguments: none
 // returns: number of penguins
 int get_nbr_of_penguins();
 
+// sets inital parameters for each player
 // aguments: pointer to array's first index and number n of players to be initialized - score, penguins left etc.
 // returns: no value 
 void init_players(struct player* array, int n);
 
+// asks user for x dimension of the map
 // arguments: none
 // returns: the size of the map's x dimension
 int get_x_dimension();
 
+// asks user for y dimension of the map
 // arguments: none
 // returns: the size of the map's y dimension
 int get_y_dimension();
 
-// each tile for the board is filled with new values
+// for each tile of the board fills it with initial values - (ocean tile or ice floe tile)
 // arguments: board, board's x and y dimension
 // returns: no value
 void generate_board(struct board_tile** board, int x, int y);
 
+// chooses the first player to start a game
 // argumenst: number of players
 // returns: random number from range 0..n-1
 int choose_first_player(int n);
 
-// for a given state of the game, decides if placement phase should be stopped = none of the players can place one of their penguins
+// for a given state of the game, decides if placement phase should be stopped - none of the players can place one of their penguins
 // arguments: board, array of players
 // returns: 1 if none of the players can place a penguin, 0 otherwise 
 int placement_game_status(struct board_tile** board, struct player* players);
@@ -39,12 +46,12 @@ int placement_game_status(struct board_tile** board, struct player* players);
 // retusn: 1 if current players can place one of their penguins, 0 otherwise
 int placement_possible(struct board_tile** board, struct player* players, int curr_player);
 
-// asks user for coordinates of placement of one of their unplaced penguins
+// asks user for coordinates of placement of one of their penguins
 // arguments: none
 // returns: placement type 
 struct placement get_placement();
 
-// decides if the placement p is possible = no other penguins on a tile and tile is an ice floe
+// decides if the placement p is possible - no other penguins on a tile and tile is an ice floe
 // agruments: board, placement
 // returns: 1 if placement is valid, 0 otherwise
 int valid_placement(struct board_tile** board, struct placement p);
@@ -54,7 +61,7 @@ int valid_placement(struct board_tile** board, struct placement p);
 // returns: no value
 void execute_placement(struct board_tile** board, struct player* players, int curr_player, struct placement p);
 
-// for a given state of the game, decides if movement phase should be stopped = none of the players can move one of their penguins
+// for a given state of the game, decides if movement phase should be stopped - none of the players can move one of their penguins
 // arguments: board, array of players
 // returns: 1 if none of the players can move one of their penguins, 0 otherwise 
 int movement_game_status(struct board_tile** board, struct player* players);
@@ -69,7 +76,7 @@ int movement_possible(struct board_tile** board, struct player* players, int cur
 // returns: movement  
 struct movement get_movement();
 
-// decides if the movement m is possible = origin tile is occupied by one of current player's penguins, ther is no other penguins on a second tile and secone tile must be an ice floe
+// decides if the movement m is possible - origin tile is occupied by one of current player's penguins, ther is no other penguins on a second tile and secone tile must be an ice floe
 // agruments: movement m, current player number
 // returns: 1 if movement is valid, 0 otherwise
 int valid_movement(struct board_tile** board, struct movement m, int curr_player);
@@ -79,10 +86,14 @@ int valid_movement(struct board_tile** board, struct movement m, int curr_player
 // returns: no value
 void execute_movement(struct board_tile** board, struct player* players, int curr_player, struct movement m);
 
-// base on the number of players takes the next one, for example (curr_player+1)%n
-void next_player(int curr_player, int n);
+// changes index of curr_player at given memory adress, for example (curr_player+1)%n
+// arguments: address to current player index, number of players in the game
+// returns: no value
+void next_player(int &curr_player, int n);
 
 // displays the board to command line
+// arguments: board, board dims
+// returns: no value
 void show_board(struct board_tile** board, int x, int y);
 
 // looks at score of every player and decides which one should win
