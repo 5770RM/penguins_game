3ea57a97cd7256ef551ab54df55def5b39231903
mjedrze5 <01168836@pw.edu.pl>
2021-11-25 14:50:08 +0100

    Upload New File

---
+++ src/T4/structures.h
@@ -0,0 +1,28 @@
+#ifndef STRUCTS
+#define STRUCTS
+struct player {
+    int id;
+    char name;
+    int fish_collected;
+    int penguins;       //number of penguins, should not be changed
+    int pen_not_placed; //number of penguins which are not placed
+};
+
+struct board_tile {
+    int occupied;   //id of player which occupies this tile or -1 if it is not occupied
+    int fishes;     //if it is equal to 0 then it is an ice floe, if it is equal to 1 or 2 or 3 then it has this amount of fish on it, otherwise it is equal to -1
+};
+
+struct coordinate {
+    int x;
+    int y;
+};
+
+struct placement {
+    struct coordinate to;
+};
+#endif
+struct movement {
+    struct coordinate from;
+    struct coordinate to;
+};
