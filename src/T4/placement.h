f8ff0aaa3baf9a9bef776d29007d62ee1068c206
syarkulo <01175695@pw.edu.pl>
2021-12-05 22:06:20 +0100

    Add new file

---
+++ src/T4/placement.h
@@ -0,0 +1,16 @@
+// check if there is left some penguins
+// arguments: number of penguins (integer)
+// returns: 1 if number of penguins equals 0; 0 otherwise 
+int placement_game_status(struct board_tile** board, struct player* players);
+
+// check if current_player has penguins
+// arguments: current_player, number of penguins (integer)
+// returns: 1 if current players has penguins, 0 otherwise
+int placement_possible(struct board_tile** board, struct player* players, int curr_player);
+
+// check if player entered valid coordinate
+// arguments: board (map), X coordinate from get_placement_x function, Y coordinate from get_placement_y function
+// returns 1 if coords valid, 0 otherwise
+int valid_placement(struct board_tile** board, struct placement p);
+
+void execute_placement(struct board_tile** board, struct player* players, int curr_player, struct placement p);
