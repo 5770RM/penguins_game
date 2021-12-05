f02f54c7ef68793c46556f8692d91691e49dbfff
Enjie Jin <01165903@pw.edu.pl>
2021-12-05 10:35:41 +0100

    Add new file

---
+++ src/T4/evaluation.h
@@ -0,0 +1,4 @@
+// looks at score of every player and decides which one should win
+// arguments: array of players, number of players
+// returns: id of the winner
+int determine_winner(struct player* players, int n);
