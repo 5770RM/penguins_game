dfb884c943addd05a48c8a0d98293bd1e658c144
Enjie Jin <01165903@pw.edu.pl>
2021-12-05 11:08:17 +0100

    Add new file

---
+++ src/T4/miscellaneous.c
@@ -0,0 +1,12 @@
+#include "structures.h"
+#include "miscellaneous.h"
+
+int choose_first_player(int n) {
+	int first_player = 1;
+	int i = 0;
+    return 1;
+}
+
+void next_player(int* curr_player, int n) {
+	curr_player = (curr_player + 1) % n;
+}
