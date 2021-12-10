3331ce3f7e4a8952d1389930a66188df2c3aeb59
sryzhkou <01175735@pw.edu.pl>
2021-12-10 20:44:21 +0100

    working bot

--- src/T4/bots.c
+++ src/T4/bots.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include "structures.h"
-int bot_possible_placement(struct board_tile **board, struct player *players, int x_size, int y_size)
+int bot_possible_placement(struct board_tile **board, struct player *players, struct bot_placement *placements, int x_size, int y_size)
 {
     srand(time(0));
-    struct bot_placement *placements = (struct bot_placement *)malloc(x_size * y_size * sizeof(struct bot_placement));
     int place_counter = 0;
     for (int i = 0; i < y_size; i++)
     {
@@ -19,26 +18,27 @@ int bot_possible_placement(struct board_tile **board, struct player *players, in
             }
         }
     }
-    return place_counter;
+    int placement_decision = (rand() % (place_counter));
+    return placement_decision;
 }
-void bot1_placement_execution(struct board_tile **board, struct player *players, struct bot_placement *placements, struct placement p, int curr_player, int place_counter)
+void bot1_placement_execution(struct board_tile **board, struct player *players, struct bot_placement *placements, struct placement p, int curr_player, int placement_decision)
 {
-    int placement_decision = (rand() % (place_counter + 1));
     p.to.x = placements[placement_decision].x;
     p.to.y = placements[placement_decision].y;
-    board[p.to.x][p.to.y].occupied = players[curr_player].id; // show player's id on the board
-    players[curr_player].pen_not_placed--;                    // reduce number of penguins of current player after every successful placement
-    players[curr_player].fish_collected++;
+    board[p.to.y][p.to.x].occupied = players[curr_player - 1].id;
+    players[curr_player - 1].fish_collected++; // show player's id on the board
+    players[curr_player - 1].pen_not_placed--; // reduce number of penguins of current player after every successful placement
+    board[p.to.y][p.to.x].fishes = 0;
 }
-int bot1_choosing_penguin(struct board_tile **board, struct player *players, int x_size, int y_size, int curr_player)
+int bot1_choosing_penguin(struct board_tile **board, struct player *players, struct bot_choosing *choice, int x_size, int y_size, int curr_player)
 {
-    struct bot_choosing *choice = (struct bot_choosing *)malloc(x_size * y_size * sizeof(struct bot_choosing));
+    srand(time(0));
     int choosing_counter = 0;
     for (int i = 0; i < y_size; i++)
     {
         for (int j = 0; j < x_size; j++)
         {
-            if (board[i][j].occupied == players[curr_player].id)
+            if (board[i][j].occupied == players[curr_player - 1].id)
             {
                 choice[choosing_counter].x = j;
                 choice[choosing_counter].y = i;
@@ -46,27 +46,29 @@ int bot1_choosing_penguin(struct board_tile **board, struct player *players, int
             }
         }
     }
-    int penguin_decision = (rand() % (choosing_counter + 1));
+    int penguin_decision = (rand() % (choosing_counter));
     return penguin_decision;
 }
-void bot1_choosing_execution(struct board_tile **board, struct player *players, struct bot_choosing *choice, struct movement m, int curr_player, int choosing_counter)
+struct movement bot1_choosing_execution(struct board_tile **board, struct player *players, struct bot_choosing *choice, struct movement m, int curr_player, int penguin_decision)
 {
-    int penguin_decision = (rand() % (choosing_counter + 1));
     m.from.x = choice[penguin_decision].x;
     m.from.y = choice[penguin_decision].y;
+    return m;
 }
-int bot1_choosing_movement(struct board_tile **board, struct player *players, struct movement m, int x_size, int y_size, int curr_player)
+int bot1_choosing_movement(struct board_tile **board, struct player *players, struct movement m, struct bot_movement *mov_choice, int x_size, int y_size, int curr_player)
 {
+    srand(time(0));
     int movement_counter = 0;
-    int up = 0;
-    int down = 0;
-    int right = 0;
-    int left = 0;
-    struct bot_movement *mov_choice = (struct bot_movement *)malloc(x_size * y_size * sizeof(struct bot_movement));
+    int up = 1;
+    int down = 1;
+    int right = 1;
+    int left = 1;
+
     while ((board[m.from.y + up][m.from.x].fishes != 0) && (board[m.from.y + up][m.from.x].occupied == 0))
     {
         mov_choice[movement_counter].x = (m.from.x);
         mov_choice[movement_counter].y = (m.from.y + up);
+        printf("%d %d",mov_choice[movement_counter].y, mov_choice[movement_counter].x);
         up++;
         movement_counter++;
     }
@@ -91,11 +93,17 @@ int bot1_choosing_movement(struct board_tile **board, struct player *players, st
         left++;
         movement_counter++;
     }
-    int movement_decision = (rand() % (movement_counter + 1));
+    printf("%d ", movement_counter); 
+    int movement_decision = (rand() % (movement_counter));
+    printf("%d ", movement_decision); // del
     return movement_decision;
 }
-void bot_mov_choosing_execution(struct board_tile **board, struct player *players, struct bot_movement *mov_choice, struct movement m, int curr_player, int movement_decision)
+struct movement bot_mov_choosing_execution(struct board_tile **board, struct player *players, struct bot_movement *mov_choice, struct movement m, int curr_player, int movement_decision)
 {
+    
     m.to.x = mov_choice[movement_decision].x;
     m.to.y = mov_choice[movement_decision].y;
+    printf("%d %d",m.to.x,m.to.y );
+    printf("%d %d",mov_choice[movement_decision].x,mov_choice[movement_decision].y );
+    return m;
 }
