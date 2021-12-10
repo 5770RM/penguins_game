4e5b4cc7f61840136986fe1e777229757436093d
sryzhkou <01175735@pw.edu.pl>
2021-12-10 22:10:43 +0100

    Update map.h

--- src/T4/map.h
+++ src/T4/map.h
@@ -6,5 +6,6 @@ void show_board(struct board_tile** board, int x, int y);
 // arguments: board, board's x and y dimension
 // returns: no value
 void generate_board(struct board_tile** board, int x, int y);
-
+// checks if there are enough tiles with 1 fish on it for the game to be playable 
+void board_gen_check(struct board_tile **board,struct player* players, int x_size, int y_size,int n)
 #endif
