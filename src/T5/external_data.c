4ab92e5cc65ec36407131251c94dddce903fd984
tgafurov <01175682@pw.edu.pl>
2022-01-03 12:03:32 +0100

    Update documentation

--- src/T5/external_data.c
+++ src/T5/external_data.c
@@ -1,10 +1,17 @@
 //
 // Created by timur on 1/3/22.
 //
+
+// input parameters: char* name
+// return: state of the game in the file
+// reads content of the file given in name, returns the state of the game given in the file
 struct GameState* read_board(char* name) {
 
 }
 
+// input parameters: struct GameState* state, char* name
+// return: none
+// writes a new file to location specified in name
 void write_board(struct GameState* state, char* name) {
 
 }
