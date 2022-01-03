85b9e47c3fd6cf5e0230caf6ca00d441b4ea8eeb
tgafurov <01175682@pw.edu.pl>
2022-01-03 11:28:26 +0100

    Create autonomous.c

---
+++ src/T5/autonomous.c
@@ -0,0 +1,12 @@
+//
+// Created by timur on 1/3/22.
+//
+#include "autonomous.h"
+
+void execute_autonomous_command(struct GameState* state, int argc, char **argv) {
+
+}
+
+int invalid_cla(int argc, char **argv) {
+
+}
