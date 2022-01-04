776e3c817ddf57fbd7ca52dfb1c9bd480ca84aad
mjedrze5 <01168836@pw.edu.pl>
2022-01-04 16:54:10 +0100

    Testing external data oi

--- src/T5/main.c
+++ src/T5/main.c
@@ -16,14 +16,18 @@
 int main(int argc, char **argv) {
     srand(time(NULL));   
 #if AUTONOMOUS_MODE
-    if (invalid_cla(argc, argv) == TRUE) {
+/*    if (invalid_cla(argc, argv) == TRUE) {
         error("Invalid command line arguments %s\n", argv[0]);
              return 1;
     }
-    struct GameState* state = malloc(sizeof(GameState));
-    read_board(state, argv[3]);
-    execute_autonomous_command(state, argc, argv); 
-    write_board(state, argv[4]);  
+*/
+    char *in = "2.txt";
+    char *out = "2out.txt"; 
+    struct GameState* state = malloc(sizeof(struct GameState));
+    read_board(state, in);
+    //read_board(state, argv[3]);
+    //execute_autonomous_command(state, argc, argv); 
+    write_board(state, out);  
 //    free_game_state(state);
 #endif
 #if INTERACTIVE_MODE  
