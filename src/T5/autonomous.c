ba48bbc95419ae81426ca33b8a4c70a0a40faeb2
mjedrze5 <01168836@pw.edu.pl>
2022-01-06 20:06:16 +0100

    Fixes for autonmous mode

--- src/T5/autonomous.c
+++ src/T5/autonomous.c
@@ -1,7 +1,10 @@
 //
 // Created by timur on 1/3/22.
 //
-#include "autonomous.h"
+#include "defines.h"
+#include "structures.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 void execute_autonomous_command(struct GameState* state, int argc, char **argv) {
@@ -16,23 +19,26 @@ void invalid_cla_check(int argc, char **argv) {
         exit( CLA_FAILURE );
 	}
 	//placement
-	if (argc == 5) {
+	else if (argc == 5) {
 		if (strcmp(argv[1], "phase=placement") != 0) {
 			fprintf(stderr, "[ERROR] Invalid command arguments! First argument is should be phase=placement.Possibly too many arguments.\n");
             exit( CLA_FAILURE );
 		}
-		char* nptr;
-		strstr(argv[2], "penguins=", nptr);
+		char* nptr = strstr(argv[2], "penguins=");
 		if (nptr == NULL) {
 			fprintf(stderr, "[ERROR] Invalid command arguments! The number of penguins is incorrect!\n");
             exit( CLA_FAILURE );
 		}
 	}
 	//movement
-	if (argc == 4) {
+	else if (argc == 4) {
 		if (strcmp(argv[1], "phase=movement") != 0) {
 			fprintf(stderr, "[ERROR]Invalid command arguments! First argument is should be phase=movement.Possibly too few arguments.\n");
             exit( CLA_FAILURE );
 		}
 	}
+    else {
+	    fprintf(stderr, "[ERROR]Invalid command arguments! Wrong number of arguments.\n");
+        exit( CLA_FAILURE );   
+    }
 }
