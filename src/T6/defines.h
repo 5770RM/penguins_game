797a437aa886ff5714d2345b184f327846c64af3
mjedrze5 <01168836@pw.edu.pl>
2022-01-19 01:51:07 +0100

    Switch to autonomous mode

--- src/T6/defines.h
+++ src/T6/defines.h
@@ -3,8 +3,8 @@
 #define TRUE 1
 #define FALSE 0
 #define SEED 0
-#define AUTONOMOUS_MODE 0
-#define INTERACTIVE_MODE 1
+#define AUTONOMOUS_MODE 1
+#define INTERACTIVE_MODE 0
 #define GUI 1
 
 #define CLA_FAILURE 1
